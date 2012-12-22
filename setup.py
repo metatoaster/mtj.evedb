3d11a261306cec8a356e4d18999da475a8565f49
Tommy Yu <y@metatoaster.com>
2012-12-22 17:19:28 +1300

    Adding mtj as the top-level namespace.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys, os
 
 version = '0.0'
 
-setup(name='evedb',
+setup(name='mtj.evedb',
       version=version,
       description="EVE DB access helper",
       long_description="""\
@@ -11,10 +11,11 @@ setup(name='evedb',
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='Tommy Yu',
-      author_email='',
+      author_email='y@metatoaster.com',
       url='',
       license='',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      namespace_packages=['mtj'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
